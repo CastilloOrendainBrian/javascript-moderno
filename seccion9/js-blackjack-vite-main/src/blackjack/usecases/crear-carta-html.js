c300e282912d54dd8384265cf836c4d9b16adb93
CastilloOrendainBrian <orendain_brian@outlook.com>
2025-04-15 12:37:55 -0600

    add env from img

--- seccion9/js-blackjack-vite-main/src/blackjack/usecases/crear-carta-html.js
+++ seccion9/js-blackjack-vite-main/src/blackjack/usecases/crear-carta-html.js
@@ -8,9 +8,8 @@ export const crearCartaHTML = ( carta ) => {
 
     if ( !carta ) throw new Error('La carta es un argumento obligatorio');
 
-    // <img class="carta" src="assets/cartas/2C.png">
     const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `${process.env.BASE_URL}assets/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
 
     return imgCarta;
--- seccion9/js-blackjack-vite-main/vite.config.js
+++ seccion9/js-blackjack-vite-main/vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     base: "/javascript-moderno/",
     plugins: [],
+    define: {
+        'process.env.BASE_URL': JSON.stringify('/javascript-moderno/')
+    }
 });
